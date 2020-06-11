2236895f298d220fc2eb0ade0de2fda4b3ac9093
ivslvs <irnvslvs@gmail.com>
2020-06-11 17:26:30 +0300

    Add token auth

--- clients/tests.py
+++ clients/tests.py
@@ -1,14 +1,17 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.test.client import Client
+
 from users.models import User
 
 
 class LoginTestCase(APITestCase):
 
     def setUp(self):
-        User.objects.create_user(email='anna@email.com', first_name='Anna', last_name='Reed',
-                                 passport_number='678qwert', password='i-keep-running', username="anna")
+        user = User.objects.create_user(email='anna@email.com', first_name='Anna', last_name='Reed',
+                                        passport_number='678qwert', password='i-keep-running', username="anna")
         User.objects.filter(id=1).update(is_active=True)
 
     def test_all_fields(self):
@@ -31,11 +34,15 @@ class LoginTestCase(APITestCase):
 class ClientBalanceTestCase(APITestCase):
     url = reverse('balance', kwargs={'pk': 1})
 
-    def test_client_balance(self):
-        # TypeError: Field 'id' expected a number but got <django.contrib.auth.models.AnonymousUser object at 0x7f76c85ea470>.
+    url_token = 'http://127.0.0.1:8000/rest-auth/login/'
 
+    def test_client_balance(self):
         User.objects.create_user(username='marie', email='marie@email.com', first_name='Marie', last_name='Snyder',
                                  passport_number='123qwer', password='i-keep-jumping')
-        response = self.client.get(self.url)
+        self.user_token = self.client.post(self.url_token, data={'username': 'marie', 'password': "i-keep-jumping"},
+                                           format='json')
+        self.token_key = self.user_token.data['key']
+
+        response = self.client.get(self.url, HTTP_AUTHORIZATION='Token ' + self.token_key)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, {"client": 1, "balance": "0.0000000000"})
--- managers/tests.py
+++ managers/tests.py
@@ -12,38 +12,46 @@ class ActivationDeactivationTestCase(APITestCase):
 
     url = 'http://testserver'
 
+    url_token = 'http://127.0.0.1:8000/rest-auth/login/'
+
     def setUp(self):
         User.objects.create_user(email='anna@email.com', first_name='Anna', last_name='Reed',
                                  passport_number='678qwert', password='i-keep-running', username="anna")
         User.objects.create_user(email='peter@email.com', first_name='Peter', last_name='Snow',
                                  passport_number='987qwert', password='i-keep-swimming', username="peter")
-        User.objects.create_user(email='dan@email.com', first_name='Dan', last_name='Tomson',
+        User.objects.create_user(email='dan@email.com', first_name='Dan', last_name='Snow',
                                  passport_number='62772qwert', password='i-keep-jumping', username="dan")
+        User.objects.filter(username='anna').update(is_superuser=True, is_staff=True)
+        User.objects.filter(last_name='Snow').update(is_active=False, status='RA')
+        self.superuser_token = self.client.post(self.url_token, data={'username': 'anna', 'password': "i-keep-running"},
+                                                format='json')
+        self.token_key = self.superuser_token.data['key']
 
     def test_activation(self):
-        User.objects.filter(is_active=True).update(is_active=False, status='RA')
-        response = self.client.get(self.activation_deactivation_list + '?status=RA')
+        response = self.client.get(self.activation_deactivation_list + '?status=RA',
+                                   HTTP_AUTHORIZATION='Token ' + self.token_key, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, [{"id": 1, "email": "anna@email.com"},
-                                         {"id": 2, "email": "peter@email.com"},
+        self.assertEqual(response.data, [{"id": 2, "email": "peter@email.com"},
                                          {"id": 3, "email": "dan@email.com"}])
 
     def test_client_activation(self):
-        response = self.client.put(self.client_activation, {"email": "anna@email.com"}, format='json')
+        response = self.client.put(self.client_activation, {"email": "anna@email.com"},
+                                   HTTP_AUTHORIZATION='Token ' + self.token_key, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, {"id": 1, "status": "A", "is_active": True})
 
     def test_deactivation(self):
-        User.objects.filter(status='A').update(status='RD', is_active=True)
-        response = self.client.get(self.activation_deactivation_list + '?status=RD')
+        User.objects.filter(last_name='Snow').update(status='RD', is_active=True)
+        response = self.client.get(self.activation_deactivation_list + '?status=RD',
+                                   HTTP_AUTHORIZATION='Token ' + self.token_key)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, [{"id": 1, "email": "anna@email.com"},
-                                         {"id": 2, "email": "peter@email.com"},
+        self.assertEqual(response.data, [{"id": 2, "email": "peter@email.com"},
                                          {"id": 3, "email": "dan@email.com"}])
 
     def test_client_deactivation(self):
         User.objects.filter(id=2).update(status='RD')
-        response = self.client.put(self.client_deactivation, {"email": "peter@email.com"}, format='json')
+        response = self.client.put(self.client_deactivation, {"email": "peter@email.com"},
+                                   HTTP_AUTHORIZATION='Token ' + self.token_key, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_client_deletion(self):
@@ -53,6 +61,6 @@ class ActivationDeactivationTestCase(APITestCase):
             "password": "i-keep-running",
             "passport_number": "678qwert"
         }
-        response = self.client.put(self.deletion, data, format='json')
+        response = self.client.put(self.deletion, data, HTTP_AUTHORIZATION='Token ' + self.token_key, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, {"id": 1, "email": "anna@email.com"})
