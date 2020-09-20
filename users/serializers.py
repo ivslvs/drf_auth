dd67d6c7c7e05d7f94d7024e87d99d6403b9926c
ivslvs <irnvslvs@gmail.com>
2020-09-20 13:30:01 +0300

    Add logging

--- users/serializers.py
+++ users/serializers.py
@@ -1,15 +1,17 @@
+from django.core.mail import send_mail
+from django.conf import settings
 from rest_framework import serializers
 from .models import User
 import logging
-from django.core.mail import send_mail
-from django.conf import settings
 
-logging.basicConfig(filename='registration.log')
+
+logging.basicConfig(filename='register.log', level=logging.DEBUG,
+                    format='%(levelname)s - %(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 logger = logging.getLogger(__name__)
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
-    """User's registration"""
+    """User registration"""
     is_active = serializers.HiddenField(default=False)
     status = serializers.HiddenField(default=User.ACTIVATION)
 
@@ -19,11 +21,12 @@ class RegistrationSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        logger.warn('Something wrong!')
+        logger.info(f'{validated_data["email"]} - User registration')
         user = super().create(validated_data)
         user.set_password(validated_data['password'])
         user.save()
 
+        # sending email to Manager for client's confirmation
         # send_mail(subject='Registration confirmation', message="Please, confirm new client's registration.",
         #           recipient_list=[settings.EMAIL_HOST_USER], from_email='new_client@email.com')
 
